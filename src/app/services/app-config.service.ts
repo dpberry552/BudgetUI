b36e53a4babc1c6b44a3272eac534c3346d98f54
dpberry552 <dpberry178@gmail.com>
2019-05-10 22:51:26 -0400

    add config file support

---
+++ src/app/services/app-config.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppConfigService {
+
+  private appConfig: any;
+
+  constructor(private http: HttpClient) { }
+
+  loadAppConfig() {
+    return this.http.get('/assets/config.json')
+      .toPromise()
+      .then(data => {
+        this.appConfig = data;
+      });
+  }
+
+  get apiBaseUrl() {
+
+    if (!this.appConfig) {
+      throw Error('Config file not loaded!');
+    }
+
+    return this.appConfig.apiBaseUrl;
+  }
+
+  get accountURL() {
+    if (!this.appConfig) {
+      throw Error('Config file not loaded!');
+    }
+
+    return this.appConfig.accountURL;
+  }
+}
