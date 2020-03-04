56c8c3ce6da3ac2a4388b548b4cf8f113872d5ee
dpberry552 <dpberry178@gmail.com>
2020-03-03 21:54:23 -0500

    re-work transaction component to include expandable table

--- src/app/components/transaction/transaction.component.ts
+++ src/app/components/transaction/transaction.component.ts
@@ -1,16 +1,27 @@
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TransactionService } from 'src/app/services/transaction.service';
 import { Transaction } from 'src/app/models/transaction';
 import { BudgetUser } from 'src/app/models/budgetUser';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
+/**
+ * @title Table with expandable rows
+ */
 @Component({
   selector: 'app-transaction',
   templateUrl: './transaction.component.html',
-  styleUrls: ['./transaction.component.css']
+  styleUrls: ['./transaction.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
-export class TransactionComponent implements OnInit {
-
+export class TransactionComponent {
   constructor(private route: ActivatedRoute, private tranService: TransactionService) {
     this.route.params.subscribe( params => { 
       this.user = JSON.parse(localStorage.getItem('curUser'));
@@ -19,14 +30,12 @@ export class TransactionComponent implements OnInit {
         this.getTransactionsForAccount(params.id);
       }
     });
-  } 
+  }
 
   transactions: Transaction[];
   fields: string[] = ['Description', 'Type', 'Amount', 'Date'];
   user: BudgetUser;
-
-  ngOnInit() {
-  }
+  expandedTransaction: Transaction | null
 
   getTransactionsForAccount(accountId: string) {
     this.tranService.getTransactionsForAccount(accountId).then((t: Transaction[]) => {
@@ -35,5 +44,4 @@ export class TransactionComponent implements OnInit {
       console.log(this.transactions);
     });
   }
- 
 }
